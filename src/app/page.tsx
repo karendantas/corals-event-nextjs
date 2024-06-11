7a135c8a34409b2d6539b6c3e361f49369529572
Karen Dantas <karennobre74@gmail.com>
2024-06-11 15:20:23 -0300

    refactor: fix background image for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
 import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 
+import backgroundimg from '@/assets/backgroundimage.jpg';
+
+const backgroundStyle = {
+  backgroundColor: 'rgb(154, 166, 192)',
+  backgroundImage: `url(${backgroundimg.src})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundBlendMode: 'multiply',
+
+}
+
 export default function Home() {
   return (
-    <main className="max-w-screen-lg h-screen mx-auto flex items-center justify-center md: px-6">
-      <div className="flex flex-1 flex-col items-start justify-center gap-2">
-        <strong>1° edição do evento técnológico da fenda do Bikini</strong>
-        <h1 className=" text-coral text-6xl font-bold"> Desafios ecológicos: Branqueamento de corais </h1>
-        <p
-          className="max-w-[600px] text-justify mt-4 text-md" 
-        > Cadastre-se no evento para criar e publicar seus artigos! O tema da edição desse ano aborda o branqueamento de corais: problemáticas e possíveis soluções </p>
-
-        <button 
-          className="bg-slate-900 border-[2px] border-coral rounded-md max-w-52 w-[100%] flex items-center justify-center gap-3 py-3 text-xl mt-12 hover:bg-coral hover:text-slate-900"
-          >
-
-          Participe 
-          <ArrowRight size={24} />
+    <main className="w-full h-screen" style={backgroundStyle}>
+      <div className="max-w-screen-lg h-screen mx-auto flex items-center justify-center md: px-6">
+        <div className="flex flex-1 flex-col items-start justify-center gap-2">
+          <strong>1° edição do evento técnológico da fenda do Bikini</strong>
+          <h1 className=" text-coral text-6xl font-bold"> Desafios ecológicos: Branqueamento de corais </h1>
+          <p
+            className="max-w-[600px] text-justify mt-4 text-md" 
+          > Cadastre-se no evento para criar e publicar seus artigos! O tema da edição desse ano aborda o branqueamento de corais: problemáticas e possíveis soluções </p>
         
-        </button>
+          <button 
+            className="bg-slate-900 border-[2px] border-coral rounded-md max-w-52 w-[100%] flex items-center justify-center gap-3 py-3 text-xl mt-12 hover:bg-coral hover:text-slate-900"
+            >
+
+            Participe 
+            <ArrowRight size={24} />
+          
+          </button>
+        </div>
       </div>
      
     </main>
