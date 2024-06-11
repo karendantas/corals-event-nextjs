be7deb4c29c1cf93087aea7c7a9be430d6862a39
Karen Dantas <karennobre74@gmail.com>
2024-06-11 14:55:23 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <main className="max-w-screen-lg h-screen mx-auto flex items-center justify-center md: px-6">
+      <div className="flex flex-1 flex-col items-start justify-center gap-2">
+        <strong>1° edição do evento técnológico da fenda do Bikini</strong>
+        <h1 className=" text-coral text-6xl font-bold"> Desafios ecológicos: Branqueamento de corais </h1>
+        <p
+          className="max-w-[600px] text-justify mt-4 text-md" 
+        > Cadastre-se no evento para criar e publicar seus artigos! O tema da edição desse ano aborda o branqueamento de corais: problemáticas e possíveis soluções </p>
+
+        <button 
+          className="bg-slate-900 border-[2px] border-coral rounded-md max-w-52 w-[100%] flex items-center justify-center gap-3 py-3 text-xl mt-12 hover:bg-coral hover:text-slate-900"
+          >
+
+          Participe 
+          <ArrowRight size={24} />
+        
+        </button>
+      </div>
+     
+    </main>
   );
 }
