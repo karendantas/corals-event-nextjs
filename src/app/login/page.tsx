aa7433408bf716cdfe75e8ac9d0ae29500b1e2f7
Karen Dantas <karennobre74@gmail.com>
2024-06-17 21:00:45 -0300

    fix: adjust register and login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,27 +3,29 @@ import Link from "next/link";
 export default function Login (){
     return (
         <main className="max-w-screen-lg h-screen mx-auto flex items-center justify-center md: px-6" >
-        <div className="w-[500px] h-[500px] rounded-md p-5 flex items-center bg-coral">
+        <div className="w-[500px] h-[500px] rounded-md p-5 flex items-center border border-coral ">
       
             <form className="flex flex-col flex-1 items-center text-3xl gap-5 font-bold  ">
-                <h3 className="text-slate-800 text-4xl uppercase font-extrabold ">LOGIN</h3>
+                <h3 className="text-coral text-4xl uppercase font-extrabold ">LOGIN</h3>
      
-                <input className="bg-darkcoral  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-800" 
+                <input className="bg-slate-800  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-100" 
                     type="text" 
                     placeholder="Login"/>
 
-                <input className="bg-darkcoral  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-800" 
+                <input className="bg-slate-800  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-100" 
                     type="password" 
                     placeholder="password"/>
 
-                <button className="bg-slate-800 px-4 h-11 rounded-md text-base w-[90%] hover:bg-slate-900 font-normal">
+                <button className="bg-coral px-4 h-11 rounded-md text-base text-slate-900 w-[90%] hover:bg-darkcoral font-bold">
                     Confirmar
                 </button>
 
                 <Link 
                     href={'/register'}
-                    className="text-base font-normal text-slate-800"
-                > Não tem conta? Cadastre-se </Link>
+                    className="text-base font-normal text-coral"
+                > Não tem conta? Cadastre-se 
+                
+                </Link>
 
             </form>
     
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,35 +4,33 @@ import Link from "next/link";
 export default function Register (){
     return (
         <main className="max-w-screen-lg h-screen mx-auto flex items-center justify-center md: px-6" >
-            <div className="w-[500px] h-[500px] rounded-md p-5 flex items-center bg-coral">
+            <div className="w-[500px] h-[500px] rounded-md p-5 flex items-center border border-coral">
           
                 <form className="flex flex-col flex-1 items-center text-3xl gap-5 font-bold  ">
-                    <h3 className="text-slate-800 text-4xl uppercase font-extrabold ">Cadastro</h3>
+                    <h3 className="text-coral text-4xl uppercase font-extrabold ">Cadastro</h3>
 
-                    <input className=" bg-darkcoral  w-[90%] p-3 text-base outline-none rounded-md placeholder:text-slate-800" 
+                    <input className=" bg-slate-800  w-[90%] p-3 text-base outline-none rounded-md placeholder:text-slate-100" 
                         type="text" 
                         placeholder="Nome"/>
 
-                    <input className="bg-darkcoral  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-800" 
+                    <input className="bg-slate-800  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-100" 
                         type="text" 
                         placeholder="Login"/>
 
-                    <input className="bg-darkcoral  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-800" 
-                        type="email" 
-                        placeholder="Email"/>
-
-                    <input className="bg-darkcoral  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-800" 
+                    <input className="bg-slate-800  w-[90%] p-3  text-base outline-none rounded-md placeholder:text-slate-100" 
                         type="password" 
                         placeholder="password"/>
 
-                    <button className="bg-slate-800 px-4 h-11 rounded-md text-base w-[90%] hover:bg-slate-900 font-normal">
+                    <button className="bg-coral px-4 h-11 rounded-md text-base text-slate-900 font-bold w-[90%] hover:bg-darkcoral">
                         Confirmar
                     </button>
 
                     <Link 
                         href={'/login'}
-                        className="text-base font-normal text-slate-800"
-                    > Já tem cadastro? Faça login </Link>
+                        className="text-base font-normal text-coral"
+                    > Já tem cadastro? Faça login 
+                    
+                    </Link>
 
                 </form>
         
